279d559a57b70f3a24f1e25961642325fa03d7ae
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-02-11 03:05:27 +0000

    Default to n=9

--- app.py
+++ app.py
@@ -4,40 +4,29 @@
 if __name__ != "__main__":
     raise Exception('Must be run as a script.')
 
-# run only if run as script
-from flask import Flask, request, render_template
+from utils import generate, log
+from flask import Flask, render_template, request
 from typing import Iterable, Iterator, List
 from logging import Logger
 import sys
 import os
 import logging
-from utils import generate
-
-# Standard Library
-# import types for static typing (mypy, pycharm etc)
-
-# 3rd Party
-
-# initalise logging with sane configuration
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(levelname)s:%(asctime)s  %(message)s"
-)
-
-log: Logger = logging.getLogger()
-
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['POST'])
 def serve():
-    return generate(txt=request.get_data(), n=8, max_avg_txt_len=5000)
+    return generate(txt=request.get_data(), n=9, max_avg_txt_len=5000)
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-generate(n=8)
+
+# pre-load
+log.warn('pre-loading...')
+generate(n=9)
 
 app.run(port=3000)
