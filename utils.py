f9efde396138c85a40f978a29544e7f66dd512dd
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-02-18 13:28:20 +0000

    Carry on building API

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from os import listdir, makedirs
 from os.path import abspath, dirname, isdir, isfile, join, relpath
 from pathlib import Path
 from random import choice, choices
-from re import IGNORECASE, MULTILINE
+from re import IGNORECASE, MULTILINE, ASCII
 from sys import getsizeof
 from threading import Lock, Semaphore
 from time import time
@@ -20,6 +20,7 @@ import nltk
 from nltk import pos_tag, sent_tokenize, word_tokenize, wordpunct_tokenize
 
 AVG_CHUNK_LEN = 5
+MIN_PROB: float = 1e-4
 NO_CPUS = 4
 
 DQUOTE: int = ord(b'"')
@@ -82,33 +83,7 @@ def cached(what: str, keep=True, load=True, save=True, archive=True):
 def get_clean_regex() -> Pattern[str]:
     return re.compile(
         r'^\s*([-*]+\s*)?(chapter|\*|note|volume|section|part|[IVX]+|harry\s+potter|by\s+|(the\s+)?end)[^\n\r]*$|\r+',
-        MULTILINE | IGNORECASE)
-
-
-@cached('CHUNK_REGEX')
-def get_chunk_regex() -> Pattern[str]:
-    PUNCT_REGEX_B = r'(?P<punct>-{1,2}|[:;,"])'
-    NL_REGEX_B = r'(?P<nl>(\n\r?|\r\n?)+)'
-    SENT_END_REGEX_B = r'(?P<sent_end>(!(!!)?|\?(\?\?)?|\.(\.\.)?))'
-    WS_REGEX_B = r'(?P<ws>\s)'
-    WORD_REGEX_B = r"(?P<word>[A-Za-z]+(-[A-Za-z]+)*?('[a-z]{0,7})?)"
-    DATE_REGEX_B = r"(?P<date>([1-9]\d*)(th|st|[nr]d)|(19|20)\d{2})"
-    TIME_REGEX_B = r"(?P<time>\d+((:\d{2}){1,2}|(\.\d{2}){1,2}))"
-    return re.compile(r'(?P<token>' + r'|'.join([
-        WS_REGEX_B,
-        SENT_END_REGEX_B,
-        PUNCT_REGEX_B,
-        NL_REGEX_B,
-        WORD_REGEX_B,
-        DATE_REGEX_B,
-        TIME_REGEX_B,
-    ]) + r')', IGNORECASE)
-
-
-def chunk(txt: str) -> Iterable[Match[str]]:
-    """Chunk text (bytes) into chunks (matches).
-    """
-    return get_chunk_regex().finditer(txt)
+        MULTILINE | IGNORECASE | ASCII)
 
 
 def root_path(*parts, mkparent=True, mkdir=False, mkfile=False) -> str:
@@ -148,7 +123,8 @@ def get_text(files=[root_path('data', fname) for fname in listdir(root_path('dat
                 log.warning(str(e))
         log.debug(f'[finished] reading from {fname} (read {getsizeof(texts[-2]) / 1e6:4.2f}MB in {time() - start_file:4.2f}s)')
 
-    with ThreadPool(max_workers=NO_CPUS, thread_name_prefix='get_text') as pool:
+    with ThreadPool(max_workers=NO_CPUS,
+                    thread_name_prefix='get_text') as pool:
         for task in [pool.submit(fn=read_file, path=p) for p in files]:
             task.result()
 
@@ -170,111 +146,73 @@ def capitalize(txt: str) -> str:
         return txt
 
 
-@cached('CHUNKS')
-def get_chunks() -> List[str]:
-    """Chunks generated from <PROJECT_ROOT>/data/*.txt.
-    """
-    ms: List[Match[str]] = list(chunk(get_text()))
-    chunks = [ms[0].group(0), ms[0].group(0)]
-    # not checking for len of tokens because every token has len >= 1
-    for i in range(2, len(ms) - 1):
-        s: str = ms[i].group(0)
-        is_q = s[0] == DQUOTE
-        is_w = bool(ms[i].group('word'))
-        is_ws = bool(ms[i].group('ws'))
-        is_p = bool(ms[i].group('punct'))
-        is_nl = bool(ms[i].group('nl'))
-        is_end = bool(ms[i].group('sent_end'))
-        is_cap = 97 <= ord(s[0]) <= 122
-        if is_w and ms[i - 2].group('sent_end') and (97 <= ord(chunks[-2][0]) <= 122):
-            chunks[-2] = capitalize(chunks[-2])
-        if is_w and ms[i - 1].group('word'):
-            chunks.append('. ' if is_cap else ' ')
-        if is_w and is_cap and (ms[i - 2].group('sent_end') or ms[i - 1].group('nl')):
-            chunks.append(capitalize(s))
-            continue
-        elif (is_w and ms[i + 1].group('word')) \
-                or (is_end and not (ms[i + 1].group('ws') or ms[i + 1].group('nl'))) \
-                or (is_p and not (is_q or ms[i + 1].group('ws') or ms[i - 1].group('nl'))):
-            chunks.append(s)
-            chunks.append(' ')
-            continue
-        elif (is_nl and not ms[i - 1].group('sent_end') and not ms[i + 1].group(0)[0] == DQUOTE and ms[i + 1].group('punct')) \
-                or ((is_end or is_p or is_ws or is_p) and s == ms[i + 1].group(0)) \
-                or (is_ws and (ms[i + 1].group('sent_end') or ms[i + 1].group('nl'))):
-            continue
-        else:
-            chunks.append(s)
-    chunks.append(ms[-1].group(0))
-    return chunks
+def get_char_ps(text: Optional[str] = None) -> List[float]:
 
+    use_decorator = text is None
 
-def get_nchunk_dict(n=2, split='chunk') -> Dict[Tuple, Dict[str, float]]:
-    """Dictionary of probabilites for ngrams of len n.
-    """
-    assert 20 >= n >= 1, f'n{split} len must be in [1, 20] but got n = {n}'
+    @cached(what='CHAR_PS',
+            load=use_decorator,
+            save=use_decorator,
+            keep=use_decorator)
+    def inner() -> List[float]:
+        bag = Counter(get_text() if text is None else text)
+        counts: Union[List[int], List[float]] = [0 for _ in range(128)]
+        for k, v in bag.items():
+            code = ord(k)
+            if code < 128:
+                counts[code] = v
+        del bag
 
-    @cached(f'_{n}{"CHUNK" if split == "chunk" else "WORD" if split == "word" else "WORDPUNCT"}_DICT')
-    def inner() -> Dict[Tuple, Dict[str, float]]:
-        tokens: List[str] = get_chunks() if split == 'chunk' \
-                else get_words() if split == 'word' \
-                else get_wordpuncts()
-        ps: Dict[Tuple, Dict[str, float]] = dict()
-        for i in range(len(tokens) - n - 1):
-            words_before: Tuple = tuple(tokens[i:i + n])
-            next_word: str = tokens[i + n]
-            if words_before not in ps:
-                ps[words_before] = {next_word: 1}
-            else:
-                ps[words_before][next_word] = \
-                    ps[words_before].get(next_word, 0) + 1
-        for ngram in ps:
-            total = 0
-            for count in ps[ngram].values():
-                total += count
-            if total > 0:
-                for next_word in ps[ngram]:
-                    ps[ngram][next_word] /= total
-        return ps
+        total: int = sum(counts)
+
+        for c in range(128):
+            counts[c] /= total
+
+        return counts
 
     return inner()
 
 
-@cached('CHAR_PS')
-def get_char_ps() -> List[float]:
-    bag = Counter(get_text())
-    counts: List[int] = list(range(128))
-    for k, v in bag.items():
-        if k < 128:
-            counts[k] = v
-    ps: List[float] = [0.0 for _ in range(128)]
-    total: int = sum(counts)
-    for c in range(128):
-        ps[c] = counts[c] / total
-    return ps
-
-
-def get_nchar_dict(n=2) -> Dict[str, Dict[int, float]]:
+def get_nchar_dict(n=2, text=None) -> Dict[str, Dict[int, float]]:
     assert 20 >= n >= 1, f'nchar len must be in [1, 20] but got n = {n}'
 
-    @cached(f'_{n}CHAR_DICT')
+    @cached(what=f'_{n}CHAR_DICT', keep=(text is None), save=(text is None), load=(text is None))
     def inner() -> Dict[str, Dict[int, float]]:
-        txt: str = get_text()
+        txt: str = get_text() if text is None else text
         ps: Dict[str, Dict[int, float]] = dict()
 
         for i in range(len(txt) - n - 1):
             chars_before: str = txt[i:i + n]
             char_after: int = ord(txt[i + n])
-            if chars_before not in ps:
+            maybe_d: Optional[Dict[int, float]] = ps.get(chars_before, None)
+            if maybe_d is None:
                 ps[chars_before] = {char_after: 1}
             else:
-                ps[chars_before][char_after] = \
-                        ps[chars_before].get(char_after, 0) + 1
+                maybe_d[char_after] = maybe_d.get(char_after, 0) + 1
 
-        for nchar in ps:
+        del txt
+
+        for nchar in list(ps.keys()):
+
             total = sum(ps[nchar].values())
-            for char_after in ps[nchar]:
-                ps[nchar][char_after] /= total
+
+            # prune
+            for char_after in list(ps[nchar].keys()):
+                if ps[nchar][char_after] == 1:
+                    del ps[nchar][char_after]
+                    total -= 1
+                elif (ps[nchar][char_after] / total) < MIN_PROB:
+                    total -= ps[nchar][char_after]
+                    del ps[nchar][char_after]
+
+            # empty dict as a result of prunning
+            if len(ps[nchar]) == 0:
+                del ps[nchar]
+
+            # counts -> probs 
+            else:
+                for char_after in ps[nchar]:
+                    ps[nchar][char_after] /= total
 
         return ps
 
@@ -296,31 +234,61 @@ def get_sents() -> List[str]:
     return sent_tokenize(get_text())
 
 
-@cached('TAGGED_WORDS')
-def get_tagged_words(universal=False) -> List[Tuple[str, str]]:
-    return pos_tag(get_words(), tagset=('universal' if universal else None))
+def get_tagged_words(
+        universal=False, text: Optional[str] = None) -> List[Tuple[str, str]]:
 
+    use_decorator = text is None
 
-def get_tagged_sents(universal=False) -> List[List[Tuple[str, str]]]:
+    @cached(what=f'TAGGED{"_UNIVERSAL" if universal else ""}_WORDS',
+            keep=use_decorator,
+            load=use_decorator,
+            save=use_decorator)
+    def inner() -> List[Tuple[str, str]]:
+        return pos_tag(
+                get_words() if text is None else word_tokenize(text),
+                tagset=('universal' if universal else None))
 
-    @cached(f'{"UNIVERSAL_" if universal else ""}TAGGED_SENTS')
-    def inner():
-        return [pos_tag(s, tagset=('universal' if universal else None))
-                for s in [wordpunct_tokenize(s) for s in get_sents()]]
     return inner()
 
 
-def get_part_of_speech(tag: str, universal=False) -> Dict[str, float]:
+def get_tagged_sents(
+        universal=False,
+        split='word',
+        text: Optional[str] = None) -> List[List[Tuple[str, str]]]:
 
-    @cached(f'WORDS_{"_UNIVERSAL" if universal else ""}{tag}')
+    tokenize = word_tokenize if split == 'word' else wordpunct_tokenize
+    tagset = 'universal' if universal else None
+
+    use_decorator = text is None
+
+    @cached(what=f'{"UNIVERSAL_" if universal else ""}TAGGED_SENTS',
+            keep=use_decorator,
+            save=use_decorator,
+            load=use_decorator)
+    def inner():
+        tokenize = word_tokenize if split == 'word' else wordpunct_tokenize
+        return [pos_tag(s, tagset=tagset) for s in [tokenize(s) for s in (get_sents() if text is None else sent_tokenize(text))]]
+
+    return inner()
+
+
+def get_part_of_speech(tag: str, universal=False, text: Optional[str] = None) -> Dict[str, float]:
+
+    use_decorator = text is None
+
+    @cached(what=f'WORDS_{"_UNIVERSAL" if universal else ""}{tag}',
+            save=use_decorator,
+            load=use_decorator,
+            keep=use_decorator)
     def inner() -> Counter:
         is_word_regex = re.compile(r'^[A-Za-z]{1,}$')
         words: Union[Counter, List[str]] = []
-        for sent in get_tagged_sents(universal=universal):
+        for sent in get_tagged_sents(universal=universal, text=text):
             for pair in sent:
                 word, tag2 = pair
                 if tag == tag2 and is_word_regex.match(word):
                     words.append(word)
+        del is_word_regex
         words = Counter(words)
         total = sum(words.values())
         for w, count in words.items():
@@ -330,54 +298,18 @@ def get_part_of_speech(tag: str, universal=False) -> Dict[str, float]:
     return inner()
 
 
-@cached('ENTS_OBJECTS')
-def get_ents_objects() -> Dict[str, float]:
-    is_word_regex = re.compile(r'^[A-Za-z]{4,}$')
-    return {e: p for e, p in
-            dict(((k, v)
-                  for k, v in get_part_of_speech('NOUN', universal=True).items()
-                  if is_word_regex.match(k))).items()
-            if e.lower() == e}
+def get_tag_dict(universal=False,
+                 text: Optional[str] = None) -> Dict[str, Dict[str, float]]:
 
+    use_decorator = text is None
 
-@cached('ENTS_PEOPLE')
-def get_ents_people() -> Dict[str, float]:
-    is_word_regex = re.compile(r'^[A-Za-z]{4,}$')
-    ents = dict(((k, v)
-                 for k, v in get_part_of_speech('NOUN', universal=True).items()
-                 if is_word_regex.match(k)))
-    return {e: p
-            for e, p in ents.items()
-            if e.capitalize() == e and e.lower() not in ents}
-
-
-@cached('SENT_STRUCTS_DICT')
-def get_sent_structs_ps(universal=False) -> Dict[Tuple, float]:
-
-    def get_sent_structs() -> Generator[Tuple, None, None]:
-        buf: List[str] = []
-        for word, tag in get_tagged_words(universal=universal):
-            buf.append(tag)
-            if tag == '.':
-                yield tuple(buf)
-                buf = []
-
-    bag = Counter(get_sent_structs())
-
-    total: int = sum(bag.values())
-
-    for s in bag:
-        bag[s] /= total
-
-    return bag
-
-
-def get_tag_dict(universal=False) -> Dict[str, Dict[str, float]]:
-
-    @cached(f'TAG_{"UNIVERSAL_" if universal else ""}DICT')
+    @cached(what=f'TAG_{"UNIVERSAL_" if universal else ""}DICT',
+            load=use_decorator,
+            keep=use_decorator,
+            save=use_decorator)
     def inner() -> Dict[str, Dict[str, float]]:
         ps: Dict[str, Dict[str, Union[int, float]]] = dict()
-        for word, tag in get_tagged_words(universal=universal):
+        for word, tag in get_tagged_words(universal=universal, text=text):
             if ps.get(tag, None) is None:
                 ps[tag] = dict()
             ps[tag][word] = ps[tag].get(word, 0) + 1
@@ -391,39 +323,39 @@ def get_tag_dict(universal=False) -> Dict[str, Dict[str, float]]:
     return inner()
 
 
-def get_tags(universal=False) -> List[str]:
+def get_tags(universal=False, text: Optional[str] = None) -> List[str]:
     """Tag names (as seen by NLTK).
     Universal tags are simplified.
     """
 
-    @cached(f'TAGS{"_UNIVERSAL" if universal else ""}')
+    use_decorator = text is None
+
+    @cached(what=f'TAGS{"_UNIVERSAL" if universal else ""}',
+            load=use_decorator,
+            keep=use_decorator,
+            save=use_decorator)
     def inner() -> List[str]:
-        return list(get_tag_dict(universal=universal).keys())
+        return list(get_tag_dict(universal=universal, text=text).keys())
 
     return inner()
 
 
-def get_tag_ps(universal=False) -> List[float]:
-    """Probabilities for each tag.
+def get_word_dict(split='word', text: Optional[str] = None) -> Dict[str, float]:
+    """Dictionary of { word (str) => probability (float), ... } pairs.
     """
 
-    @cached(f'TAG{"_UNIVERSAL" if universal else ""}_PS')
-    def inner() -> List[float]:
-        return list(get_tag_dict(universal=universal).values())
+    use_decorator = text is None
 
-    return inner()
-
-
-def get_chunk_dict(split='chunk') -> Dict[str, float]:
-    """Dictionary of { chunk (str) => probability (float), ... } pairs.
-    """
-
-    @cached(f'{"CHUNK" if split == "chunk" else "WORD" if split == "word" else "WORDPUNCT"}_DICT')
+    @cached(what=f'{"WORD" if split == "word" else "WORDPUNCT"}_DICT',
+            load=use_decorator,
+            keep=use_decorator,
+            save=use_decorator)
     def inner() -> Dict[str, float]:
         ps = Counter(
-                get_chunks() if split == 'chunk'
-                else get_words() if split == 'word'
-                else get_wordpuncts())
+                get_words() if split == 'word' and text is None
+                else get_wordpuncts() if split == 'wordpunct' and text is None
+                else word_tokenize(text) if split == 'word'
+                else wordpunct_tokenize(text))
         total: int = sum(ps.values())
         for token in ps:
             ps[token] /= total
@@ -432,41 +364,97 @@ def get_chunk_dict(split='chunk') -> Dict[str, float]:
     return inner()
 
 
-def rand_sent_struct(universal=False) -> List[str]:
-    a, p = zip(*get_sent_structs_ps(universal=universal).items())
-    return choices(tuple(a), tuple(p))[0]
-
-
-def rand_sent(tagged=False, universal=False) -> Union[str, List[Tuple[str, str]]]:
+def rand_sent(tagged=False, universal=False, text: Optional[str] = None) -> Union[str, List[Tuple[str, str]]]:
     return choice(
-        get_tagged_sents(universal=universal) if tagged else get_sents())
+        get_tagged_sents(universal=universal, text=text) if tagged
+        else get_sents() if text is None
+        else sent_tokenize(text))
 
 
-def rand_word(tag: Optional[str] = None, universal=False) -> str:
-    tag_dict = get_tag_dict(universal=universal)
+def rand_word(tag: Optional[str] = None, universal=False, text: Optional[str] = None) -> str:
+    tag_dict = get_tag_dict(universal=universal, text=text)
     if tag is None:
-        tag = choices(
-                get_tags(universal=universal),
-                get_tag_ps(universal=universal))[0]
+        tag = choice(get_tags(universal=universal, text=text))
     return choices(
         tuple(tag_dict[tag].keys()),
         tuple(tag_dict[tag].values()))[0]
 
 
-def rand_ent_person() -> str:
-    return choices(
-        tuple(get_ents_people().keys()),
-        tuple(get_ents_people().values()))[0]
+def get_ents(type='object',
+             plural=None,
+             text: Optional[str] = None) -> Dict[str, float]:
+    assert type in {'object', 'person'}, \
+        f'type of entity must be: "object" OR "person"'
+
+    use_decorator = text is None
+
+    @cached(what=(f'ENTS_{"PEOPLE" if type == "person" else "OBJECTS"}_{"SINGULAR" if plural is False else "PLURAL" if plural is True else "ALL"}'),
+            save=use_decorator,
+            load=use_decorator,
+            keep=use_decorator)
+    def inner() -> Dict[str, float]:
+        ents: Dict[str, float] = get_part_of_speech(
+                'NOUN', universal=True, text=text)
+
+        def remove_all(test=(lambda e: False)):
+            for e in filter(test, list(ents.keys())):
+                p: float = ents[e]
+                del ents[e]
+                l: int = len(ents)
+                share: float = p / l
+                for other in ents:
+                    ents[other] += share
+
+        is_word_regex = re.compile(r'[A-Za-z]{4,}')
+        remove_all(lambda e: not is_word_regex.fullmatch(e))
+        del is_word_regex
 
 
-def rand_ent_object() -> str:
-    return choices(
-        tuple(get_ents_objects().keys()),
-        tuple(get_ents_objects().values()))[0]
+        if type == 'person':
+            remove_all(lambda e: e.capitalize() != e or (e.lower() in ents))
+        else:
+            remove_all(lambda e: e.capitalize() == e or (e.lower() not in ents))
+
+
+        if plural is True:
+            remove_all(lambda e: not (e.endswith('s') or e.endswith('i')))
+
+        elif plural is False:
+            remove_all(lambda e: e.endswith('s') or e.endswith('i'))
+
+        return ents
+
+    return inner()
+
+
+def rand_ent(type='object', plural=None, text: Optional[str] = None) -> str:
+    assert type in {'object', 'person'}, \
+            f'type of entity must be: "object" OR "person"'
+
+    d = get_ents(text=text, plural=plural, type=type)
+
+    xs: Tuple[str] = tuple(d.keys())
+    ps: Tuple[float] = tuple(d.values())
+
+    del d
+
+    def pick() -> str:
+        return choices(xs, ps)[0]
+
+    candidate: str = pick()
+
+    if plural is False:
+        while candidate.endswith('s') or candidate.endswith('i'):
+            candidate = pick()
+    elif plural is True:
+        while not (candidate.endswith('s') or candidate.endswith('i')):
+            candidate = pick()
+
+    return candidate
 
 
 def get_size(obj: Any, seen=None):
-    """Recursively finds size of objects.
+    """Recursively find size of an object.
     """
     size = getsizeof(obj)
     if seen is None:
